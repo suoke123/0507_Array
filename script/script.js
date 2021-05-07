defb4c851456db00fdac9b472ed3659975389b2d
suoke123 <asak222@hanmail.net>
2021-05-07 22:04:18 +0900

    0507_Array

---
+++ script/script.js
@@ -0,0 +1,16 @@
+/* 배열변수 사용법 */
+var aryData =  new Array("B", 30);   // ["A", 20];
+
+// ary = array, 배열을 의미하는 단축어
+// 배열변수명 => aryData
+// 배열변수의 데이터 개수 => 2개      Size, 크기
+// 배열변수의 순서 => 0번부터 시작   Index, 인덱스
+
+alert("인덱스0번(첫쨰값) : " + aryData[0] + "\n인덱스1번(둘재값) : " + aryData[1]);
+
+/*
+일반변수 사용법
+var data = "A";
+var no = 20;
+alert("data : " + data + "\nno : " + no);
+*/
